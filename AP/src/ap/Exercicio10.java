16f878bc76b0a420b2961d6e82db9881655a5ec9
rdg8 <digorinaldi@gmail.com>
2017-03-27 21:13:18 -0300

    lista 1 AP

---
+++ AP/src/ap/Exercicio10.java
@@ -0,0 +1,31 @@
+/*
+ * Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total 
+ * de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor 
+ * ganha 15% de comissão sobre suas vendas efetuadas, calcular e imprimir o 
+ * total a receber no final do mês.
+ */
+package ap;
+
+import java.util.Scanner;
+
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double Porcentagem=0.05d;
+                
+        System.out.print("Escreva o nome do Vendedor: ");
+        String vendedor = console.nextLine();
+        
+        System.out.print("Escreva Salário fixo: R$");
+        double salarioFixo = console.nextDouble();
+        System.out.print("Escreva as Vendas efetuadas no mês: R$");
+        double vendas = console.nextDouble();
+        
+        double salarioTotal = salarioFixo + (vendas *  Porcentagem);
+        
+        System.out.println("Salario total: R$"+salarioTotal);
+        
+        
+    }
+}
